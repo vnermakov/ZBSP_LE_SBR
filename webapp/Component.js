f2251098bbf49c34a792e248fe67ddb04286db4b
Ermakov V.N <v.n.ermakov@gmail.com>
2020-11-05 09:36:17 +0300

    [INIT]

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("ZBSP_LE_SBR.zbsp_le_sbr.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
